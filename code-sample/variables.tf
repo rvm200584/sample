c21ddd545e5740ee301f518f6498912768a468c4
rvm200584 <137646104+rvm200584@users.noreply.github.com>
2026-02-24 21:34:00 +0530

    Define project_name and notification_email variables
    Added variables for project name and notification email.

--- code-sample/variables.tf
+++ code-sample/variables.tf
@@ -1,1 +1,9 @@
+cat > variables.tf << 'EOF'
+variable "project_name" {
+  default = "logicworks"
+}
 
+variable "notification_email" {
+  default = "your-email@gmail.com"   # ← CHANGE THIS TO YOUR EMAIL
+}
+EOF
